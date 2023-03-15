ed55db2f5576e01d039e2cbf97e03c1ea49d686f
A.J. Marchese <aj.marchese@outlook.com>
2023-03-14 17:09:00 -0700

    Add project files.

---
+++ mdi-forms/MDIParent1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdi_forms
+{
+    public partial class MDIParent1 : Form
+    {
+
+        public MDIParent1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            //Form childForm = new Form();
+            //childForm.MdiParent = this;
+            //childForm.Text = "Window " + childFormNumber++;
+            //childForm.Show();
+            //making the Windows form as the child of the MDI
+
+            Form newForm = new Form1();
+
+            newForm.MdiParent = this;
+
+            newForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void MDIParent1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
